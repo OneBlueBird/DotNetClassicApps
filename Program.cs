e9eb7d52650b4aab01166b74b8e5a01927bf9280
Saravanakumar.M <saravanakumar.murugesan@gmail.com>
2020-05-21 19:27:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudifyClassicApps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			try
+			{
+				Console.WriteLine("Hello classic dotnet");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception occurred inside Main method : " + ex.Message);
+			}
+        }
+    }
+}
